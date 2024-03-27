32d03013657ad66cb4c1d1baa6213035623cdc7c
arifzaidpp <arifzaidaiju@gmail.com>
2024-03-27 22:03:26 +0530

    almost completed with socket.io also

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -7,9 +8,12 @@ import messageRoutes from "./routes/message.routes.js";
 import usersRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -20,13 +24,20 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", usersRoutes);
 
+
+app.use(express.static(path.join(__dirname,"/fronted/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
+
 // app.get("/", (req, res) => {
 //     // root route http://localhost:5000/
 //     res.send("Hello Arif")
 // });
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`)   
 });
