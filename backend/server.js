667c0a699227c467d4829183f08ef197bd9f7051
Arif Zaid P P <arifzaidaiju@gmail.com>
2024-03-29 13:55:27 +0530

    Spelling mistake changed

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/message", messageRoutes);
 app.use("/api/users", usersRoutes);
 
 
-app.use(express.static(path.join(__dirname,"/fronted/dist")))
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
