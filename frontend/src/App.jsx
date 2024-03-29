30d123d3ae022f74eccecd3d3f7469e8068c8d7c
Arif Zaid P P <arifzaidaiju@gmail.com>
2024-03-29 13:23:15 +0530

    BrowserRouter as added in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
